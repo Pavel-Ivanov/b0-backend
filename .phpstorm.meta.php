4c1e63f45d2330f1151779718807f39c6257ae21
Pavel Ivanov <p.ivanov.spb@gmail.com>
2020-04-05 18:09:14 +0300

    Изменены настройки проекта

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-29 12:08:34.
+    * Generated on 2020-04-05 15:07:39.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
