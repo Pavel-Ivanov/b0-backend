172a4b5b248b40b723582b03801019d1287d1c20
Pavel Ivanov <p.ivanov.spb@gmail.com>
2020-04-05 17:10:02 +0300

    Добавлены компоненты для форм- text и textarea

---
+++ app/View/Components/Form/TextareaField.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Components\Form;
+
+use Illuminate\View\Component;
+
+class TextareaField extends Component
+{
+    public $name;
+    public $label;
+
+    /**
+     * @param string $name
+     * @param string $label
+     */
+    public function __construct($name, $label)
+    {
+        $this->name = $name;
+        $this->label = $label;
+    }
+
+    /**
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return view('components.form.-textarea-field');
+    }
+}
